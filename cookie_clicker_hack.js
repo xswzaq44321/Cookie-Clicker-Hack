2764a17bb87dea4128adaf86a4968dd1d94c269e
xswzaq44321 <xswzaq44321@gmail.com>
2018-10-18 15:44:50 +0800

    Create cookie_clicker_hack.js

---
+++ cookie_clicker_hack.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name         cookie clicker hack
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  automating cookie production
+// @author       You
+// @match        http://orteil.dashnet.org/cookieclicker/
+// @require      http://code.jquery.com/jquery-3.3.1.min.js
+// @grant        none
+// ==/UserScript==
+
+$(document).ready(function() {
+    setInterval(function(){ Game.ClickCookie();}, 10);
+    console.log("autoClicked");
+    setInterval(function(){
+        var toBuy = null, max = 0;
+        if(toBuy == null){
+            for(var i = 0; i <= 14; i++){
+                var me = Game.ObjectsById[i];
+                if(me == null){
+                    continue;
+                }
+                if(max < me.storedCps / me.price){
+                    max = me.storedCps / me.price;
+                    toBuy = me;
+                }
+            }
+        }
+        var childs = document.getElementById("upgrades");
+        for(i = 0; i < childs.childElementCount; i++){
+            var child = childs.childNodes[i];
+            var data = child.getAttribute("onclick").split("[")[1].split("]");
+            if(child != null && Game.UpgradesById[data[0]].basePrice < toBuy.price){
+                child.onclick();
+                console.log("buy upgrade " + Game.UpgradesById[data[0]].name);
+                return;
+            }
+        }
+        console.log("buy product "+toBuy.name);
+        if(toBuy != null){
+            toBuy.buy();
+        }
+    }, 100);
+
+    setInterval(function(){
+        for(var i in Game.shimmers){
+            Game.shimmers[i].pop();
+        }
+    }, 1000);
+});
